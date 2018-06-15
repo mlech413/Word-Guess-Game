134704cc18ef2e8aba37db3d7ad1796644b852d3
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-06-14 20:43:31 -0400

    just cosmetic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -76,63 +76,53 @@ var computerChoice = [
  ,"Redskins"
     ];
 
-// Randomly chooses a choice from the word array computerChoices.
+// --Randomly chooses a choice from the word array computerChoices.
 computerWord = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 console.log("start: " + computerWord);
 
-
-
-
-// Create a new empty array, with num of items in array matching num of letters in computer selected word.
+// --Create a new empty array, with num of items in array matching num of letters in computer selected word.
 var wordToGuess = new Array(computerWord.length);
 console.log(wordToGuess);
 
-// Set initial display array for the word to be all empty '_' spaces.
+// --Set initial display array for the word to be all empty '_' spaces.
 for (var i = 0; i < wordToGuess.length; i++) {
     wordToGuess[i] = "_"
 }
 // console.log(wordToGuess);
 
-
-
+// --Display initial html text at top of screen at start.
 var topHtml =
 "<div class='row'>" +
-    "<div class='col-sm-6 offset-sm-3'>" +
+    "<div class='col-sm-10 offset-sm-1'>" +
         "<br><br><h2>NFL TEAMS Word Guess Game</h2>" +
-        "<h2>Press any letter to get started!</h2>";
+        "<h3>Press any letter to get started!</h3>";
   
-// Create display html text for display of blank word at start.
+// --Create display html text for display of the blank word at start.
 var blankLinesHtml = "<h1>"
 for (var w = 0; w < wordToGuess.length; w++) {
     blankLinesHtml = blankLinesHtml + wordToGuess[w] + " ";
 }
 var openingHtml = blankLinesHtml + "</h1></div></div>"; 
 
-
-// Set the inner HTML contents of the #game div to our html string
+// --Print to the screen the first lines and blank word.
 document.querySelector("#game").innerHTML = topHtml + openingHtml;
 
-// This function is run whenever the user presses a key.
+// --The user presses a key.
 document.onkeyup = function(event) {
 
-    // Determines which key was pressed.
+    // --Determines which key was pressed.
     var userGuess = event.key;
-    // Reset indicators
+    // --Reset some indicators.
     validLetter = false;
     currentPickCorrect = false;
-    // Skip this word selection process if the computer already picked a word.
-    // if (computerPickMade == "n") {
-    //     Randomly chooses a choice from the word array computerChoices.
-    //     computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    //     computerPickMade = "y";
-    // }
+    // --Set the computer-picked word and anything entered by the user to all uppercase.
     computerWord = computerWord.toUpperCase();
     console.log("computerWord: " + computerWord);
-    var userGuess = userGuess.toUpperCase();
+    userGuess = userGuess.toUpperCase();
     console.log("userGuess: " + userGuess);
 
 
-    // Check the alphabet array to see if a valid letter was chosen.
+    // --Check the alphabet array to see if a valid letter was chosen.
     for (var v = 0; v < alphabet.length; v++) {
         if (userGuess == alphabet[v]) {
             validLetter = true;
@@ -140,38 +130,46 @@ document.onkeyup = function(event) {
     }
     // console.log("validLetter: " + validLetter)
 
-    // Only proceed if a valid letter is chosen.
+    // --Only proceed if a valid letter is chosen.
     if (validLetter) {
 
-        // Match the userGuess letter to each letter of computerWord.
+        // --Loop through each letter of the computerWord and match against the user input userGuess.
         for (var i = 0; i < computerWord.length; i++) {         
-            console.log("userGuess: " + userGuess);
-            console.log("computerWord.charAt(i): " + computerWord.charAt(i));
-            // If correct guess, the userGuess matches one of the letters in the computer's word, so store the location
-            // of that letter within the word.
+            // console.log("userGuess: " + userGuess);
+            // console.log("computerWord.charAt(i): " + computerWord.charAt(i));
+            // --Set indicator if a correct guess, meaning the userGuess matches one of the letters in the computer's word.
             if (userGuess == computerWord.charAt(i)) {
-                console.log("if (userGuess == computerWord.charAt(i)): " + userGuess + ", " + computerWord.charAt(i));
+                currentPickCorrect = true;
+                // console.log("if (userGuess == computerWord.charAt(i)): " + userGuess + ", " + computerWord.charAt(i));
+                // --Check to see if this correct guess was already picked previously - if so set an indicator.
                 if (wordToGuess[i] != "_") {
                     alreadyPicked = true;
-                    console.log("alreadyPicked: " + alreadyPicked);
+                    //console.log("alreadyPicked: " + alreadyPicked);
                 }
-                currentPickCorrect = true;
+                // --Move correct guess into the array so that the letter doesn't display as blank ('_') anymore.
                 wordToGuess[i] = userGuess;
+                
                 // console.log("Set wordToGuess[i] = userGuess:" + wordToGuess[i]);
                 // console.log("wordToGuess: " + wordToGuess);
             }
         }
         
+        // --If at least one letter was already incorrectly picked:
         if (pickedLetters.length > 0) {
-
+            // --Check to see if the user input was already selected and stored in the pickedLetters array.
+            // --pickedLetters array is the array holding all of the incorrect guesses already made.
             for (var z = 0; z < pickedLetters.length; z++) {
+                // --Each item in the picked letter array has a letter followed by a space (for display purposes),
+                // --so move just the single-charactor letter to compareLetter so it can be evaluated. 
                 var compareLetter = pickedLetters[z].charAt(1);
                 // console.log("DUPLICATE CHECKER LOOP");
                 // console.log("alreadyPicked: " + alreadyPicked);
                 // console.log("pickedLetters[z]: " + pickedLetters[z]);
                 // console.log("compareLetter: " + compareLetter);
                 // console.log("userGuess: " + userGuess);
+                // Compare each previous incorrect letter against the user input letter, to see if it was already selected.
                 if (compareLetter == userGuess) {
+                    // --Letter was alreaddy selected, so set an indicator.
                     alreadyPicked = true;
                     // console.log("compareLetter == userGuess");
                     // console.log("alreadyPicked: " + alreadyPicked);
@@ -180,53 +178,67 @@ document.onkeyup = function(event) {
         }   
 
         console.log("currentPickCorrect: " + currentPickCorrect);
+        // --User input a letter that was previously selected.
         if (alreadyPicked) {
             pickResult = "You already guessed that!"
             // console.log("wordToGuess: " + wordToGuess);
         }
+        // --User guessed a correct letter.
         else if (currentPickCorrect) {
             pickResult = "CORRECT!";
+            // --Each letter guessed could be in the word multiple times (e.g. guessing P for apple has 2 P's).
             for (var d = 0; d < computerWord.length; d++) {
                 if (userGuess == computerWord.charAt(d)) 
-                correctGuesses++;
+                    // --Increment counter to know how many total letters have actually been guessed in the word, for use later.
+                    correctGuesses++;
                 // console.log("correctGuesses: " + correctGuesses);
             }
         }
+        // --User guess was incorrect, remove one remaining guess.
         else {
             pickResult = "WRONG!";
+            // --Remove one remaining guess.
             guessesRemaining--;
-            
-            // console.log("p: " + p);
-            // console.log("pickedLetters[p]: " + pickedLetters[p]);
+            // --Move the incorrect guess to the array holding all of the incorrect guesses so far.
             pickedLetters[p]  = " " + userGuess;
             // console.log("pickedLetters[p]: " + pickedLetters[p]);
             // console.log("pickedLetters: " + pickedLetters);
+            // --Increment index p so pickedLetters array is ready to accept the next incorrect guess.
             p++;
             // console.log("wordToGuess[i]: " + wordToGuess[i]);
             // console.log("userGuess: " + userGuess);
         }
-
+        
+        // --A few lines ago the total letters have actually been guessed in the word were set, so now use that values to
+        // --be able to see if all of the letters in the word have been guessed. If they have, process winning logic.
         if (correctGuesses >= computerWord.length) {
             finalMessage = computerWord + ": YOU WIN!!!!!";
+            // --Show team logo.
             finalImage = "<img id='teamPic' src='assets/images/" + computerWord + ".jpg'>";
+            // --Add one to number of wins.
             wins++;
+            // --Set indicator for game over, to be used later.
             gameOver = true;
-            // Reset initial display array for the word to be all empty '_' spaces.
+            // --Reset initial display array for the word to be all blank '_' spaces again.
             for (var i = 0; i < wordToGuess.length; i++) {
                 wordToGuess[i] = "_"
             }
             // console.log(wordToGuess);
         }
+        // --No more guesses remain, so losing logic.
         else if (guessesRemaining <= 0) {
             finalMessage = computerWord + ": YOU LOSE!";
+            // --Show team logo.
             finalImage = "<img id='teamPic' src='assets/images/" + computerWord + ".jpg'>";
+            // --Add one to number of loses.
             losses++;
+            // --Set indicator for game over, to be used later.
             gameOver = true;
-            // Reset initial display array for the word to be all empty '_' spaces.
+            // --Reset initial display array for the word to be all blank '_' spaces again.
             for (var i = 0; i < wordToGuess.length; i++) {
                 wordToGuess[i] = "_"
             }
-            console.log(wordToGuess);
+            //console.log(wordToGuess);
         }
         // console.log("finalMessage: " + finalMessage);
         // console.log("correctGuesses: " + correctGuesses);
@@ -236,21 +248,24 @@ document.onkeyup = function(event) {
         // console.log("Wins: " + wins);
         // console.log("Losses: " + losses);
 
-        
+        // --Write out the final displays:
+
+        // --The game is over, so write out all blank '_' again so the refreshed word is ready and displayed to play again.
         var linesHtml = "<h1>"
         for (var w = 0; w < wordToGuess.length; w++) {
             linesHtml = linesHtml + wordToGuess[w] + " ";
         }
             linesHtml + "</h1></div>";
 
+        // --The rest of the final displays, showing result and the team pic, plus overall wins and losses.
         var mainHtml =            
             "<div class='row'>" +
-                "<div class='col-sm-2 offset-sm-1'>" +
+                "<div class='col-sm-2'>" +
                     finalImage + 
                 "</div>" +
-                "<div class='col-sm-6'>" +
+                "<div class='col-sm-8'>" +
                     "<br><h1>" + pickResult + "</h1>" +
-                    "<h3>You picked:</h3><h3>" + pickedLetters + "</h3>" +
+                    "<h2>You picked:</h2><h2>" + pickedLetters + "</h2>" +
                     "<br><h2>Guesses remaining: " + guessesRemaining + "</h2>" +
                     "<h2>Wins: " + wins + "</h2>" +
                     "<h2>Losses: " + losses + "</h2>" +
@@ -260,14 +275,16 @@ document.onkeyup = function(event) {
                     finalImage + 
                 "</div>" +
             "</div>"
-        // Set the inner HTML contents of the #game div to our html string
+
         document.querySelector("#game").innerHTML = topHtml + linesHtml + mainHtml;
 
 
         }
 
+        // --Reset this indicator for the next game.
         alreadyPicked = false;
 
+        // --Reset some more indicators and write out the next word and screen for the next game.
         if (gameOver) {
             computerPickMade = 'n';
             validLetter = false;
@@ -279,20 +296,20 @@ document.onkeyup = function(event) {
             p = 0;
             pickedLetters = [];
             
-            // Randomly chooses a choice from the word array computerChoices.
+            // --Randomly chooses from the word array computerChoices. Same logic as directly above and also in beginning.
             computerWord = computerChoice[Math.floor(Math.random() * computerChoice.length)];
             console.log("start: " + computerWord);
 
-            // Create a new empty array, with num of items in array matching num of letters in computer selected word.
+            // --Create a new empty array, with num of items in array matching num of letters in computer selected word.
             wordToGuess = new Array(computerWord.length);
             console.log(wordToGuess);
 
-            // Set initial display array for the word to be all empty '_' spaces.
+            // --Set initial display array for the word to be all empty '_' spaces.
             for (var i = 0; i < wordToGuess.length; i++) {
                 wordToGuess[i] = "_"
             }
             console.log("At end, wordToGuess: " + wordToGuess);
-            // Create display html text for display of blank word at start.
+            // --Create display html text for display of blank word at start.
             var blankLinesHtml = "<h2>"
             for (var w = 0; w < wordToGuess.length; w++) {
                 blankLinesHtml = blankLinesHtml + wordToGuess[w] + " ";
@@ -306,10 +323,10 @@ document.onkeyup = function(event) {
 
             var mainHtml =            
             "<div class='row'>" +
-                "<div class='col-sm-2 offset-sm-1'>" +
+                "<div class='col-sm-2'>" +
                     finalImage + 
                 "</div>" +
-                "<div class='col-sm-6'>" +
+                "<div class='col-sm-8'>" +
                     "<br><h1>" + pickResult + "</h1>" +
                     "<h3>You picked:</h3><h3>" + pickedLetters + "</h3>" +
                     "<br><h2>Guesses remaining: " + guessesRemaining + "</h2>" +
@@ -321,7 +338,7 @@ document.onkeyup = function(event) {
                     finalImage + 
                 "</div>" +
             "</div>"
-            // Set the inner HTML contents of the #game div to our html string
+
             document.querySelector("#game").innerHTML = topHtml + linesHtml + mainHtml;
     
             
